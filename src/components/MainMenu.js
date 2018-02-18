5f64b79155c7063ef8263682e1666311f47ec73c
bong kim <32095134+underbleu@users.noreply.github.com>
2018-02-18 23:49:54 +0900

    기능 추가 - 메인메뉴

---
+++ src/components/MainMenu.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import { Menu, Icon } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import { COLOR } from 'constants/Color'
+
+const Wrapper = styled.div`
+  text-align: center;
+  background-color: ${COLOR.MAIN_RED};
+`
+const MainMenu = () => (
+  <Wrapper>
+    <Menu secondary inverted>
+      <Menu.Item
+        name="댓댓"
+        as={Link}
+        to="/"
+      />
+      <Menu.Item
+        name="messages"
+        position="right"
+        as={Link}
+        to="/mypage"
+      >
+        <Icon name="user" />
+      </Menu.Item>
+    </Menu>
+  </Wrapper>
+)
+
+export default MainMenu
+
